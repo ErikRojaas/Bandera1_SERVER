e075f1afbbf806793fde2142acefce82a5d7252d
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2025-04-24 16:25:19 +0200

    changed import

--- gameLogic.js
+++ gameLogic.js
@@ -4,7 +4,7 @@ const Player = require('./player.js');
 const Room = require('./room.js');
 const Key = require ('./key.js')
 const WebClient = require('./webClient.js');
-const { insertPlayer } = require('./CreateColections.js');
+const { insertPlayer } = require('./mongoUtils.js');
 const nodemailer = require('nodemailer');
 // Conectar a MongoDB
 class GameLogic {
