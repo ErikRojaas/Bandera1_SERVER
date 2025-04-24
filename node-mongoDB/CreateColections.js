250f78b252feb8d0b065a5fa4fd91ad8337c2040
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2025-04-24 15:40:58 +0200

    changed models

--- node-mongoDB/CreateColections.js
+++ node-mongoDB/CreateColections.js
@@ -71,10 +71,11 @@ jugadorSchema.pre('save', async function (next) {
 });
 
 // Crear modelos con los nombres de colecciones específicas
-const Jugador = mongoose.models.Jugadores  mongoose.model('Jugadores', jugadorSchema, 'Jugadores');
-const HistorialPartida = mongoose.models.HistorialPartidas  mongoose.model('HistorialPartidas', partidoSchema, 'HistorialPartidas');
-const Equipo = mongoose.models.Equipos  mongoose.model('Equipos', equipoSchema, 'Equipos');
-const JugadoresEnEquipo = mongoose.models.JugadoresEnEquipo  mongoose.model('JugadoresEnEquipo', jugadorEquipoSchema, 'JugadoresEnEquipo');
+const jugadores = mongoose.models.Jugadores || mongoose.model('Jugadores', jugadorSchema, 'Jugadores');
+const historialPartida = mongoose.models.HistorialPartidas || mongoose.model('HistorialPartidas', partidoSchema, 'HistorialPartidas');
+const equipos = mongoose.models.Equipos || mongoose.model('Equipos', equipoSchema, 'Equipos');
+const jugadoresEnEquipo = mongoose.models.JugadoresEnEquipo || mongoose.model('JugadoresEnEquipo', jugadorEquipoSchema, 'JugadoresEnEquipo');
+
 
 // Insertar datos de prueba
 async function insertarDatos() {
