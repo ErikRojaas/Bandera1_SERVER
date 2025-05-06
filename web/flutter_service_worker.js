b069b1ad049100a7c86ce939c982e4478b988d0a
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2025-05-06 20:55:14 +0200

    web

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -60,7 +60,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "26436d2c3b090ff0d32b9cd296f2d391
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "725397c152b33a253c88ea3437ced924",
+"flutter_bootstrap.js": "3455a044a911f94ca9a19ee464e29157",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
